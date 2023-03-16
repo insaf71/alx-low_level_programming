e973482579b73f69a7b6bd1a40f39647b5f6303d
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-16 14:26:05 -0700

    Puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+/*returns an integer value of 0 to indicate successful program execution.*/
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
 return (0);
 }
-
