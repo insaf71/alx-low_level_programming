e985ca032f1ee63e468556c481094b1c48eca9de
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-17 02:57:43 -0700

    Size c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,5 +13,5 @@ printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 printf("Size of a float: %ld byte(s)\n", sizeof(float));
 printf("Size of a double: %ld byte(s)\n", sizeof(double));
 printf("Size of a long double: %ld byte(s)\n", sizeof(long double));
-return 0;
+return (0);
 }
