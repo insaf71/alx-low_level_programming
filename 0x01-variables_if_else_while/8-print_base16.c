9792d14b55725dbe833e16633d72dd7857378db0
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-20 09:17:44 -0700

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char j;
+
+for (i = 0; i < 10; i++)
+	putchar(i);
+for (j = 'a'; j < 'f'; j++)
+	putchar(j);
+putchar('\n');
+retunr (0);
+}
