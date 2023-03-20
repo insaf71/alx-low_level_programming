13a7ca14b5f8631166ea28d282322ff37fa562d2
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-20 16:25:54 -0700

    Print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,14 +14,14 @@ int k;
 
 for (i = 0; i < 10; i++)
 {
-	for (j = 0; j < 10; j++)
+	for (j = 1; j < 10; j++)
 	{
-		for (k = 0; k < 10; k++)
+		for (k = 2; k < 10; k++)
 		{
 			putchar(i +'0');
 			putchar(j +'0');
 			putchar(k +'0');
-			if (i+j+k != 24)
+			if (i + j + k != 24)
 			{
 				putchar(',');
 				putchar(' ');
