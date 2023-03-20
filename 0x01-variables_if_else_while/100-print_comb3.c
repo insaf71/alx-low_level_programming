acd75c88767db967ba1b467a21f55d489ad4b84c
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-20 10:22:23 -0700

    Print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+	for (j = 1; j < 10; j++)
+	{
+		if (i < j; i != j)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+		}
+		putchar(',');
+		putchar(' ');
+	}
+putchar('\n');
+return (0);
+}
