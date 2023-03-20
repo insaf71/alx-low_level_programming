030dd445786cbe0dc18bbf18ffe54d3b40f2e289
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-20 10:27:52 -0700

    Print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,7 +13,7 @@ for (i = 0; i < 10; i++)
 {
 	for (j = 1; j < 10; j++)
 	{
-		if (i < j, i != j)
+		if (i < j && i != j)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
@@ -21,6 +21,7 @@ for (i = 0; i < 10; i++)
 		putchar(',');
 		putchar(' ');
 	}
+}
 putchar('\n');
 return (0);
 }
