73a0094dc7d3ea0b934ab0d7dfea3cbee16c1c22
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-20 16:23:32 -0700

    Print comb3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+int j;
+int k;
+
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j < 10; j++)
+	{
+		for (k = 0; k < 10; k++)
+		{
+			putchar(i +'0');
+			putchar(j +'0');
+			putchar(k +'0');
+			if (i+j+k != 24)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+return (0);
+}
