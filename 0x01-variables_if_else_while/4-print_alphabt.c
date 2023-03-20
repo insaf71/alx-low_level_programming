bbffb4459686547430b3f21608166534c0cc061f
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-20 07:54:24 -0700

    Print alphabets except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char low, e, q;
+
+e = 'e';
+q = 'q';
+
+for (low = 'a'; low <= 'a'; low++)
+{
+	if (low =! e && low =! q)
+	{
+		putchar(low);
+	}
+}
+putchar('\n');
+return(0);
+}
