839206a91006c6339817f94682e83064ebae97be
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-20 16:54:33 -0700

    Print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,9 +20,9 @@ for (i = 0; i < 10; i++)
 		{
 			if (i < j && j < k)
 			{
-				putchar(i +'0');
-				putchar(j +'0');
-				putchar(k +'0');
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 				if (i + j + k != 24)
 				{
 					putchar(',');
@@ -32,5 +32,6 @@ for (i = 0; i < 10; i++)
 		}
 	}
 }
+putchar('\n');
 return (0);
 }
