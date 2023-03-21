3dc9b421f85606e2da2e000806978a01596f252c
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-21 09:12:54 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * followed by a new line
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+char ch;
+
+for(ch = 'a'; ch <= 'z'; ch++)
+{
+	_putchar(ch);
+}
+_putchar('\n');
+return (0);
+}
