579cbbb2a9fa8a07b96c83873bf385864d8599e6
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-21 08:00:50 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - script of the main function  _putchar
+ * main - script of the main function
  * _putchar: print the alphabet putchar
  * c: The character to print
  *  Return: always 0 (Success)
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	_putchar('_');
+	_putchar("_");
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
