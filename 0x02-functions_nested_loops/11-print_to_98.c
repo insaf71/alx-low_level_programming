40fab80a02ff81ad072254a9394d9814e20e862a
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-22 17:34:10 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-			{	
+			{
 				printf("%d, ", i);
 			}
 			else if (i == 98)
@@ -27,7 +27,7 @@ void print_to_98(int n)
 			}
 		}
 	}
-       	else if (n >= 98)
+	else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
