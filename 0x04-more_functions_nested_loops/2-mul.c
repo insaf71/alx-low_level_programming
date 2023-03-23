357b264f40c3fff372d9150e3a63b6d3eb95f998
insaf71 <i.benmouloud@enim.ac.ma>
2023-03-23 04:11:29 -0700

    multiplying the numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - pultiplies two numbers
+ * @a: the first number to multiply
+ * @b: the second number to multiply with the first
+ * Return: the result of the multuiplication between a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
