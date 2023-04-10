a516e9348c7fe0ed4edbd512035007f8c53a66a1
insaf71 <i.benmouloud@enim.ac.ma>
2023-04-10 07:43:50 -0700

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,9 +5,9 @@
  * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: Always 0 (Success)		
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
 	int i;
 
